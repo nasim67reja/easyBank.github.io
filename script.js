de2c0647786fdf1a9ca98fe1de1d23a27d0f6b61
nasim67reja <nasimreja67@gmail.com>
2022-03-04 16:25:52 -0800

    small addition to my page content

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function reportWindowSize() {
 }
 reportWindowSize();
 window.addEventListener("resize", reportWindowSize);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+const hambergur = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const mainNav = document.querySelector(".main-nav-list");
+
+hambergur.addEventListener("click", function (e) {
+  mainNav.classList.add("nav-open");
+  hambergur.style.display = "none";
+  close.style.display = "inline-block";
+});
+
+close.addEventListener("click", function () {
+  mainNav.classList.remove("nav-open");
+  hambergur.style.display = "inline-block";
+  close.style.display = "none";
+});
